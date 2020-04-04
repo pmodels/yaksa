ecb3271c33e466006d67686f07d4b1522da5eaf5
Hui Zhou <hzhou321@anl.gov>
2020-04-04 15:36:59 -0500

    runtests: add verbose option
    Adding an verbose option to allow us seem more info, especially the make
    action, for debugging purpose.
    
    Signed-off-by: Hui Zhou <hzhou321@anl.gov>

--- test/runtests.py
+++ test/runtests.py
@@ -21,6 +21,8 @@ class colors:
     OTHER   = '\033[1;35m' # purple
     END     = '\033[0m'    # reset
 
+opts = {"verbose": 0}
+
 # junit variables
 num_tests = 0
 num_failures = 0
@@ -158,9 +160,13 @@ def run_testlist(testlist):
         # make executable
         ############################################################################
         execname = line.split(' ', 1)[0].rstrip()
+        if opts['verbose']:
+            print("make " + execname)
         p = subprocess.Popen(['make', execname], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         ret = wait_with_signal(p)
         out = p.communicate()
+        if opts['verbose']:
+            print(out[0].decode().strip())
         if (ret != 0):
             print(colors.FAILURE + "\n==== \"make %s\" output ====" % execname + colors.END)
             print(out[0].decode().strip())
@@ -190,6 +196,9 @@ if __name__ == '__main__':
     parser.add_argument('testlists', help='testlist files to execute', nargs='+')
     parser.add_argument('--summary', help='file to write the summary to', required=True)
     args = parser.parse_args()
+
+    if os.environ.get('V'):
+        opts["verbose"] = 1
 
     for testlist in args.testlists:
         run_testlist(os.path.abspath(testlist))
