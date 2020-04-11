306ba5b1acbb5cd79ad5101b4dc0e3b5c196f6df
Pavan Balaji <balaji@anl.gov>
2020-04-11 02:12:07 +0000

    tests: separate tests into different testlists
    This makes it easier to run the testlists separately on different
    nodes.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- maint/gentests.py
+++ maint/gentests.py
@@ -13,15 +13,6 @@ counts = [ 17, 1075, 65536 ]
 types = [ "int", "short_int", "int:3,double:2" ]
 seed = 1
 
-##### create empty testlist to facilitate optional appending
-def create_testlist(testlist):
-    try:
-        outfile = open(testlist, "w")
-    except:
-        sys.stderr.write("error creating testlist %s\n" % testlist)
-        sys.exit()
-    outfile.close()
-
 ##### simple tests generator
 def gen_simple_tests(testlist):
     global seed
@@ -41,7 +32,7 @@ def gen_simple_tests(testlist):
     sys.stdout.write("done\n")
 
 ##### pack/iov tests generator
-def gen_pack_iov_tests(fn, testlist, create, extra_args):
+def gen_pack_iov_tests(fn, testlist, extra_args):
     global seed
 
     segments = [ 1, 64 ]
@@ -51,10 +42,7 @@ def gen_pack_iov_tests(fn, testlist, create, extra_args):
     prefix = os.path.dirname(testlist)
 
     try:
-        if (create == "create"):
-            outfile = open(testlist, "w")
-        else:
-            outfile = open(testlist, "a")
+        outfile = open(testlist, "w")
     except:
         sys.stderr.write("error creating testlist %s\n" % testlist)
         sys.exit()
@@ -136,19 +124,18 @@ def gen_flatten_tests(testlist):
 if __name__ == '__main__':
     gen_simple_tests("test/simple/testlist.gen")
 
-    gen_pack_iov_tests("pack", "test/pack/testlist.gen", "create", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.gen", \
                        " -sbuf-memtype unreg-host -tbuf-memtype unreg-host -dbuf-memtype unreg-host")
-    create_testlist("test/pack/testlist.cuda.gen")
-    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.gen", "append", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.d-d-d.gen", \
                        " -sbuf-memtype device -tbuf-memtype device -dbuf-memtype device")
-    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.gen", "append", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.d-rh-d.gen", \
                        " -sbuf-memtype device -tbuf-memtype reg-host -dbuf-memtype device")
-    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.gen", "append", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.d-urh-d.gen", \
                        " -sbuf-memtype device -tbuf-memtype unreg-host -dbuf-memtype device")
-    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.gen", "append", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.rh-d-rh.gen", \
                        " -sbuf-memtype reg-host -tbuf-memtype device -dbuf-memtype reg-host")
-    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.gen", "append", \
+    gen_pack_iov_tests("pack", "test/pack/testlist.cuda.urh-d-urh.gen", \
                        " -sbuf-memtype unreg-host -tbuf-memtype device -dbuf-memtype unreg-host")
 
-    gen_pack_iov_tests("iov", "test/iov/testlist.gen", "create", "")
+    gen_pack_iov_tests("iov", "test/iov/testlist.gen", "")
     gen_flatten_tests("test/flatten/testlist.gen")
--- test/pack/Makefile.cuda.mk
+++ test/pack/Makefile.cuda.mk
@@ -3,5 +3,11 @@
 ##     See COPYRIGHT in top-level directory
 ##
 
-pack_testlists += $(top_srcdir)/test/pack/testlist.cuda.gen
-EXTRA_DIST += $(top_srcdir)/test/pack/testlist.cuda.gen
+cuda_pack_testlists = $(top_srcdir)/test/pack/testlist.cuda.d-d-d.gen \
+	$(top_srcdir)/test/pack/testlist.cuda.d-rh-d.gen \
+	$(top_srcdir)/test/pack/testlist.cuda.d-urh-d.gen \
+	$(top_srcdir)/test/pack/testlist.cuda.rh-d-rh.gen \
+	$(top_srcdir)/test/pack/testlist.cuda.urh-d-urh.gen
+
+pack_testlists += $(cuda_pack_testlists)
+EXTRA_DIST += $(cuda_pack_testlists)
