993a724d5591d1e60306e8ee219b05fb2ec350ce
Pavan Balaji <balaji@anl.gov>
2020-05-05 22:50:45 -0500

    build: avoid installing yaksa for embedded builds
    This is needed on systems where installing the library disables
    embedding the library.  See pmodels/mpich#4496.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/frontend/include/Makefile.mk
+++ src/frontend/include/Makefile.mk
@@ -5,8 +5,13 @@
 
 AM_CPPFLAGS += -I$(top_srcdir)/src/frontend/include -I$(top_builddir)/src/frontend/include
 
+if EMBEDDED_BUILD
+noinst_HEADERS += \
+	src/frontend/include/yaksa.h
+else
 include_HEADERS += \
 	src/frontend/include/yaksa.h
+endif !EMBEDDED_BUILD
 
 noinst_HEADERS += \
 	src/frontend/include/yaksi.h
