ba81956bb0ac1dbd339961bf6c37a7228bbb9202
Pavan Balaji <balaji@anl.gov>
2020-08-02 02:09:51 +0000

    pools: use utlist and uthash to manage chunk lists
    This fix improves adding new chunks to the pool a constant-time
    operation.  It also simplifies the code somewhat.
    
    Signed-off-by: Pavan Balaji <balaji@anl.gov>

--- src/util/yaksu_pool.c
+++ src/util/yaksu_pool.c
@@ -9,11 +9,48 @@
 #include <assert.h>
 #include <stdint.h>
 #include <string.h>
+#include <yutlist.h>
+#include <yuthash.h>
+
+/*
+ * The pool contains three lists/hashmaps:
+ *
+ *  1. chunks -- this is the list of chunks that were allocated.  This
+ *     is mainly to keep track of the start addresses of the buffers
+ *     that were allocated, so we can eventually free them.
+ *
+ *  2. free_elems -- this is a doubly-linked list of free elements.
+ *     Whenever a new chunk is allocated, it is split into a number of
+ *     elements and each element is added to this list.  This makes
+ *     element allocation O(1) time, when there is a free element
+ *     available.  When no free element is available, we need
+ *     O(elements_per_chunk) time to add all the elements in the new
+ *     chunk to this list.
+ *
+ *  3. used_elems -- this is a hashmap that allows us to lookup the
+ *     element address based on the user buffer address.  This makes
+ *     freeing an element O(1) time (assuming that the hashmap has no
+ *     conflicts), because we can simply lookup the element address
+ *     based on the element ID or buffer address and then add it back
+ *     to the free_elems list.
+ */
+
+typedef struct elem {
+    int id;
+    void *buf;
+
+    /* for the free list */
+    struct elem *next;
+    struct elem *prev;
+
+    /* for the used list */
+    UT_hash_handle hh;
+} elem_s;
 
 typedef struct chunk {
-    void **elems;
     void *slab;
     struct chunk *next;
+    struct chunk *prev;
 } chunk_s;
 
 typedef struct pool_head {
@@ -29,6 +66,9 @@ typedef struct pool_head {
     unsigned int current_num_chunks;
     unsigned int max_num_chunks;
     chunk_s *chunks;
+
+    elem_s *free_elems;         /* list of free elements */
+    elem_s *used_elems;         /* hash table of used elements */
 } pool_head_s;
 
 static pthread_mutex_t global_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -55,6 +95,8 @@ int yaksu_pool_alloc(uintptr_t elemsize, unsigned int elems_in_chunk, unsigned i
     pool_head->current_num_chunks = 0;
     pool_head->max_num_chunks = (maxelems / elems_in_chunk) + ! !(maxelems % elems_in_chunk);
     pool_head->chunks = NULL;
+    pool_head->free_elems = NULL;
+    pool_head->used_elems = NULL;
 
     *pool = (void *) pool_head;
 
@@ -69,27 +111,31 @@ int yaksu_pool_free(yaksu_pool_s pool)
 
     pthread_mutex_lock(&global_mutex);
 
-    int count = 0;
-    for (chunk_s * chunk = pool_head->chunks; chunk;) {
-        chunk_s *next = chunk->next;
-        for (unsigned int i = 0; i < pool_head->elems_in_chunk; i++)
-            if (chunk->elems[i])
-                count++;
+    /* throw a warning if the used hashmap is not empty */
+    int count = HASH_COUNT(pool_head->used_elems);
+    if (count) {
+        fprintf(stderr, "[WARNING] yaksa: %d leaked handles\n", count);
+        fflush(stderr);
+    }
 
+    /* free the freelist elements */
+    elem_s *el, *el_tmp;
+    DL_FOREACH_SAFE(pool_head->free_elems, el, el_tmp) {
+        DL_DELETE(pool_head->free_elems, el);
+        free(el);
+    }
+
+    /* free the buffers associated with each chunk */
+    chunk_s *chunk, *tmp;
+    DL_FOREACH_SAFE(pool_head->chunks, chunk, tmp) {
+        DL_DELETE(pool_head->chunks, chunk);
         pool_head->free_fn(chunk->slab);
-        free(chunk->elems);
         free(chunk);
-        chunk = next;
     }
 
     /* free self */
     pthread_mutex_destroy(&pool_head->mutex);
     free(pool_head);
-
-    if (count) {
-        fprintf(stderr, "[WARNING] yaksa: %d leaked handles\n", count);
-        fflush(stderr);
-    }
 
     pthread_mutex_unlock(&global_mutex);
     return rc;
@@ -99,70 +145,60 @@ int yaksu_pool_elem_alloc(yaksu_pool_s pool, void **elem, unsigned int *elem_idx
 {
     int rc = YAKSA_SUCCESS;
     pool_head_s *pool_head = (pool_head_s *) pool;
-    int idx;
-    chunk_s *new = NULL;
 
     pthread_mutex_lock(&pool_head->mutex);
 
-    /* try to find an available type */
-    idx = 0;
-    for (chunk_s * chunk = pool_head->chunks; chunk; chunk = chunk->next) {
-        for (unsigned int i = 0; i < pool_head->elems_in_chunk; i++) {
-            if (chunk->elems[i] == NULL) {
-                chunk->elems[i] = (char *) chunk->slab + i * pool_head->elemsize;
-                YAKSU_ERR_CHKANDJUMP(!chunk->elems[i], rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
-                *elem_idx = idx;
-                *elem = chunk->elems[i];
-                goto fn_exit;
-            }
-            idx++;
+    chunk_s *chunk = NULL;
+    if (pool_head->free_elems == NULL) {
+        /* no more free elements left; see if we can allocate more
+         * chunks */
+        assert(pool_head->current_num_chunks <= pool_head->max_num_chunks);
+        if (pool_head->current_num_chunks == pool_head->max_num_chunks)
+            goto fn_exit;
+
+        /* allocate a new chunk */
+        chunk = (chunk_s *) malloc(sizeof(chunk_s));
+        YAKSU_ERR_CHKANDJUMP(!chunk, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
+
+        chunk->slab = pool_head->malloc_fn(pool_head->elems_in_chunk * pool_head->elemsize);
+        YAKSU_ERR_CHKANDJUMP(!chunk->slab, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
+
+        /* keep track of the allocated chunk */
+        DL_APPEND(pool_head->chunks, chunk);
+
+        /* append each element to the free list */
+        for (int i = 0; i < pool_head->elems_in_chunk; i++) {
+            elem_s *el = (elem_s *) malloc(sizeof(elem_s));
+            YAKSU_ERR_CHKANDJUMP(!el, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
+
+            el->buf = (char *) chunk->slab + (i * pool_head->elemsize);
+            el->id = pool_head->current_num_chunks * pool_head->elems_in_chunk + i;
+            DL_APPEND(pool_head->free_elems, el);
         }
+
+        pool_head->current_num_chunks++;
     }
 
-    /* no empty slot available; see if we can allocate more chunks */
-    assert(pool_head->current_num_chunks <= pool_head->max_num_chunks);
-    if (pool_head->current_num_chunks == pool_head->max_num_chunks)
-        goto fn_exit;
+    /* we should now have some free elements */
+    assert(pool_head->free_elems);
 
-    /* allocate another chunk */
-    idx = 0;
-    new = (chunk_s *) malloc(sizeof(chunk_s));
-    YAKSU_ERR_CHKANDJUMP(!new, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
+    /* delete the first element in the free list and add it to the
+     * used hash */
+    elem_s *el;
+    el = pool_head->free_elems;
+    DL_DELETE(pool_head->free_elems, el);
+    HASH_ADD_INT(pool_head->used_elems, id, el);
 
-    new->slab = pool_head->malloc_fn(pool_head->elems_in_chunk * pool_head->elemsize);
-    YAKSU_ERR_CHKANDJUMP(!new->slab, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
-
-    new->elems = (void **) malloc(pool_head->elems_in_chunk * sizeof(void *));
-    YAKSU_ERR_CHKANDJUMP(!new->elems, rc, YAKSA_ERR__OUT_OF_MEM, fn_fail);
-
-    memset(new->elems, 0, pool_head->elems_in_chunk * sizeof(void *));
-    new->next = NULL;
-
-    pool_head->current_num_chunks++;
-
-    if (pool_head->chunks == NULL) {
-        pool_head->chunks = new;
-    } else {
-        chunk_s *chunk;
-        idx += pool_head->elems_in_chunk;
-        for (chunk = pool_head->chunks; chunk->next; chunk = chunk->next) {
-            idx += pool_head->elems_in_chunk;
-        }
-        chunk->next = new;
-    }
-
-    new->elems[0] = new->slab;
-    *elem_idx = idx;
-    *elem = new->elems[0];
+    *elem_idx = el->id;
+    *elem = el->buf;
 
   fn_exit:
     pthread_mutex_unlock(&pool_head->mutex);
     return rc;
   fn_fail:
-    if (new) {
-        free(new->elems);
-        free(new->slab);
-        free(new);
+    if (chunk) {
+        free(chunk->slab);
+        free(chunk);
     }
     goto fn_exit;
 }
@@ -174,33 +210,30 @@ int yaksu_pool_elem_free(yaksu_pool_s pool, unsigned int idx)
 
     pthread_mutex_lock(&pool_head->mutex);
 
-    chunk_s *chunk = pool_head->chunks;
-    while (idx >= pool_head->elems_in_chunk) {
-        assert(chunk->next);
-        chunk = chunk->next;
-        idx -= pool_head->elems_in_chunk;
-    }
+    /* find the element in the used hashmap */
+    elem_s *el;
+    HASH_FIND_INT(pool_head->used_elems, &idx, el);
+    assert(el);
 
-    chunk->elems[idx] = NULL;
+    /* delete the element from the used hashmap and add it to the free
+     * list */
+    HASH_DEL(pool_head->used_elems, el);
+    DL_PREPEND(pool_head->free_elems, el);
 
     pthread_mutex_unlock(&pool_head->mutex);
     return rc;
 }
 
-int yaksu_pool_elem_get(yaksu_pool_s pool, unsigned int elem_idx, void **elem)
+int yaksu_pool_elem_get(yaksu_pool_s pool, unsigned int idx, void **elem)
 {
     int rc = YAKSA_SUCCESS;
     pool_head_s *pool_head = (pool_head_s *) pool;
-    unsigned int idx = elem_idx;
 
-    chunk_s *chunk = pool_head->chunks;
-    while (idx >= pool_head->elems_in_chunk) {
-        assert(chunk->next);
-        chunk = chunk->next;
-        idx -= pool_head->elems_in_chunk;
-    }
+    elem_s *el = NULL;
+    HASH_FIND_INT(pool_head->used_elems, &idx, el);
+    assert(el);
 
-    *elem = chunk->elems[idx];
+    *elem = el->buf;
 
     return rc;
 }
